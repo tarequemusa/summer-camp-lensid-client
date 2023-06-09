5bc9bdafcfc3eb90b7d4bb283879e2b17eeb8ce1
tarequemusa <tareque.web@gmail.com>
2023-06-10 04:55:45 +0600

    navbar profile picture updated

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,8 +1,18 @@
 import {Link} from 'react-router-dom';
 import NavLogo from '../../../assets/lensid-main-logo.png'
+import {useContext} from 'react';
+import {AuthContext} from '../../../providers/AuthProvider';
 
 
 const NavBar = () => {
+    const {user, logOut} = useContext(AuthContext);
+
+    const handleLogOut = (event) => {
+        event.preventDefault();
+        logOut()
+            .then()
+            .catch(error => console.log(error))
+    }
 
     const navOptions =
         <>
@@ -36,20 +46,28 @@ const NavBar = () => {
                     </ul>
                 </div>
                 <div className='navbar-end'>
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    {/* <div className="navbar-end grid sm:grid-flow-col md:grid-flow-col items-center gap-2">
+                        {
+                            user &&
+                            <div className='text-center px-3 py-0'><img className='rounded-full w-3/5' title={user.displayName} src={user.photoURL} alt="" /></div>
+                        }
+                        {
+                            user ? <>
+                                <span>{user.email}</span> <Link to='/'><button onClick={handleLogOut} className='btn btn-sm'> Log Out</button></Link> </> : <button className='px-2 btn btn-sm'><Link to='/login'>Login</Link></button>
+
+                        }
+                    </div> */}
+                    <div className="dropdown dropdown-end flex items-center gap-3 justify-end">
+                        {user && <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-20 rounded-full" title={user.displayName}>
+                                <img src={user.photoURL} />
                             </div>
-                        </label>
-                        <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="justify-between">
-                                    Dashboard
-                                </a>
-                            </li>
-                            <li><a>Logout</a></li>
-                        </ul>
+                        </label>}
+                        {
+                            user ? <>
+                                <Link to='/'><button onClick={handleLogOut} className='btn btn-sm btn-secondary btn-outline rounded-full'> Log Out</button></Link> </> : <button className='px-4 btn btn-outline btn-primary rounded-xl'><Link to='/login'>Login</Link></button>
+
+                        }
                     </div>
                 </div>
             </div>
